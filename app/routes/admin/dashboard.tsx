79b374c68d48345ccfbfbcf2ada171d27f6b8599
RotarAlexC <rotaralexc@gmail.com>
2025-05-08 01:36:08 +0300

    Dasboard Layout

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,10 +1,17 @@
+import { Header } from 'components';
 import React from 'react'
 
 const Dashboard = () => {
+  const user = { name: 'Alex' };
   return (
-    <div>
-      Dashboard Page
-    </div>
+    <main className='dashboard wrapper'>
+      <Header
+        title={`Welcome ${user?.name ?? 'Guest'}`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+
+      Dashboard Page Contents
+    </main>
   )
 }
 
