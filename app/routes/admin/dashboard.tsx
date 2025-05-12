1cdddcce2ce7a1a95fbd30cb7810524831f68a9d
RotarAlexC <rotaralexc@gmail.com>
2025-05-12 17:24:38 +0300

    Auth UI

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,15 +1,19 @@
 import { Header, StatsCard, TripCard } from 'components';
 import React from 'react'
+import { getUser } from '~/appwrite/auth';
 import { dashboardStats, user, allTrips } from '~/constants';
+import type { Route } from './+types/dashboard';
 
 const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats
 
-const Dashboard = () => {
+export const clientLoader = async () => await getUser();
 
+const Dashboard = ({ loaderData }: Route.ComponentProps) => {
+  const user = loaderData as User | null;
   return (
     <main className='dashboard wrapper'>
       <Header
-        title={`Welcome ${user?.name ?? 'Guest'}`}
+        title={`Welcome ${user?.name ?? 'Guest'} 👋`}
         description="Track activity, trends and popular destinations in real time"
       />
 
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,12 +1,15 @@
-import { Link, NavLink } from "react-router"
+import { Link, NavLink, useLoaderData, useNavigate } from "react-router"
+import { logoutUser } from "~/appwrite/auth";
 import { sidebarItems } from "~/constants"
 import { cn } from "~/lib/utils"
 
 const NavItems = ( { handleClick }: { handleClick?: () => void } ) => {
-    const user = {
-        name: 'Alex',
-        email: 'contact@alexc.ro',
-        imageUrl: '/assets/images/david.webp'
+    const user = useLoaderData(); //gets the data from the nearast client loader route in this case admin-layout
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logoutUser();
+        navigate('/sign-in')
     }
 
   return (
@@ -39,7 +42,7 @@ const NavItems = ( { handleClick }: { handleClick?: () => void } ) => {
                 <p>{user?.email}</p>
             </article>
 
-            <button onClick={() => {console.log('logout')}} className="cursor-pointer">
+            <button onClick={handleLogout} className="cursor-pointer">
                 <img src="/assets/icons/logout.svg" alt="logout" className="size-6" />
             </button>
         </footer>
